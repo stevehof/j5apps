e125665ba81553d7725d931729ae601b24fa7617
Steve Hofmeyr <stevehof@users.noreply.github.com>
2014-03-12 01:03:41 +0200

    fix loader

--- j5apps.py
+++ j5apps.py
@@ -1,8 +1,7 @@
 from flask import Flask, url_for
 from jinja2 import Template, Environment, PackageLoader
-from Apps import poweralert
 import imp
-env = Environment(loader=PackageLoader('J5apps', 'templates'))
+env = Environment(loader=PackageLoader('j5apps', 'templates'))
 
 app = Flask(__name__)
 
@@ -15,8 +14,7 @@ def index():
 @app.route('/<app>/')
 def power_alert(app):
     allowed_apps = "poweralert"
-    fp, pathname, description = imp.find_module("Apps/poweralert")
-    print(pathname)
+    fp, pathname, description = imp.find_module("Apps/%s.py" % app)
     app_body = getattr(imp.load_module(app, fp, pathname, description),app)().get_app()
     body = env.get_template('app_template.html').render(title="Filter String App", app_body=app_body)
     return env.get_template('index.html').render(body=body, url_for=url_for)
